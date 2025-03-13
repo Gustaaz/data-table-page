b614a84457087c7dcd3356c6f0f8f36912ee3e63
Gustavo Guilherme <guguiantunes@hotmail.com>
2025-03-13 12:10:39 -0400

    realizando a chamada api e listando os usuarios

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,30 @@
-import { columns, type Payment } from "@/components/data-table-test/columns";
+import { columns } from "@/components/data-table-test/columns";
 import { DataTable } from "@/components/data-table-test/data-table";
+import type { Paginated } from "@/types/paginated";
+import type { User } from "@/types/user";
 
-async function getData(): Promise<Payment[]> {
-  
-  return [
-    {
-      id: "728ed52f",
-      amount: 100,
-      status: "pending",
-      email: "m@example.com",
-    },
-  ]
+
+interface Response extends Paginated{
+  users: User[]
+}
+async function getData(): Promise<Response> {
+  const res = await fetch('https://dummyjson.com/users?limit=5&skip=10&select=firstName,age')
+
+  if (!res.ok) {
+    throw new Error('Failed to fetch data')
+  }
+
+  const data = await res.json() 
+  return data
+
 }
 export default async function Home() {
   const data = await getData()
+
+  console.log(data);
   return (
     <div className=" container mx-auto mt-12">
-      <DataTable columns={columns} data={data} />
+      <DataTable columns={columns} data={data.users} />
     </div>
   );
 }
---
+++ src/types/paginated.ts
@@ -0,0 +1,5 @@
+export interface Paginated {
+  total: number;
+  skip: number;
+  limit: number;
+}
